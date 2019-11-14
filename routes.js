035adee91f836e04da507127fbe42c922f366a70
VladMB <cph-mb402@cphbusiness.dk>
2019-11-14 10:46:32 +0100

    added status route

--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const bookingRoutes = require('./routes/booking');
 const rideOfferRoutes = require("./routes/rideOffer");
 const router = express.Router();
 
+router.get("/", function(req, res) {
+    res.json('hi');
+}
 router.use('/booking', bookingRoutes);
 router.use('/ride-offers', rideOfferRoutes);
 
